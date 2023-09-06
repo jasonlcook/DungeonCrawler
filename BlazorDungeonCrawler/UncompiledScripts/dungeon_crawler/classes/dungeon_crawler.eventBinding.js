00d01084145ea194d7050743303cc032a3ca4cf0
jasonlcook <tarquinnlcook@gmail.com>
2023-09-06 02:08:05 +0100

    Add event bindings classes

---
+++ BlazorDungeonCrawler/UncompiledScripts/dungeon_crawler/classes/dungeon_crawler.eventBinding.js
@@ -0,0 +1,35 @@
+﻿//
+class EventBinding {
+    constructor() {
+        this.Dispacter = null;
+        this.Type = '';
+        this.Handler = null;
+        this.Bound = false;
+        this.Name = '';
+    }
+
+    mapListener(dispacter, type, handler, name) {
+        //Dispacter
+        if (typeof dispacter == 'undefined' || dispacter == null) {
+            dungeon_crawler.core.outputError('Unexpected error experienced whilst setting a dispacter for the event binding');
+            return false;
+        }
+        this.Dispacter = dispacter;
+
+        //Type
+        if (typeof type == 'undefined' || type == null) {
+            dungeon_crawler.core.outputError('Unexpected error experienced whilst setting a type for the event binding');
+        }
+        this.Type = type;
+
+        //Handler
+        if (typeof handler == 'undefined' || handler == null) {
+            dungeon_crawler.core.outputError('Unexpected error experienced whilst setting a handler for the event binding');
+        }
+        this.Handler = handler;
+
+        if (typeof name !== 'undefined' && name !== null) {
+            this.Name = name;
+        }
+    }
+}
---
+++ BlazorDungeonCrawler/UncompiledScripts/dungeon_crawler/classes/dungeon_crawler.eventBindings.js
@@ -0,0 +1,87 @@
+﻿//
+class EventBindings {
+    constructor() {
+        this._eventBindings = [];
+        this._eventBindingNames = [];
+    }
+
+    isEmpty() {
+        if (this._eventBindings.length == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    getElement(index) {
+        return this._eventBindings[index];
+    }
+
+    getLength() {
+        return this._eventBindings.length;
+    }
+
+    addEventBinding(dispacter, type, handler, name) {
+        if (!this._eventBindingNames.includes(name)) {
+            let eventBinding = new EventBinding();
+            eventBinding.mapListener(dispacter, type, handler, name);
+
+            dungeon_crawler.core.outputToConsole(`Registered event "${eventBinding.Name}"`);
+
+            this._eventBindings.push(eventBinding);
+
+            this._eventBindingNames.push(name);
+        } else {
+            dungeon_crawler.core.outputToConsole(`Event "${name}" already bound`);
+        }
+    }
+
+    getElementWithName(name) {
+        if (typeof this._eventBindings != 'undefined' && this._eventBindings != null && this._eventBindings.length > 0) {
+            let eventBinding
+            for (let i = 0; i < this._eventBindings.length; i++) {
+                eventBinding = this._eventBindings[i];
+
+                if (eventBinding.Name == name) {
+                    return eventBinding;
+                }
+            }
+        }
+    }
+
+    bindEvents() {
+        if (typeof this._eventBindings != 'undefined' && this._eventBindings != null && this._eventBindings.length > 0) {
+            let eventBinding
+            for (let i = 0; i < this._eventBindings.length; i++) {
+                eventBinding = this._eventBindings[i];
+
+                if (!eventBinding.Bound) {
+                    eventBinding.Dispacter.on(eventBinding.Type, eventBinding.Handler);
+                    eventBinding.Bound = true;
+
+                    dungeon_crawler.core.outputToConsole(`Event bound: "${eventBinding.Name}"`);
+                } else {
+                    dungeon_crawler.core.outputToConsole(`Event "${eventBinding.Name}" already bound.`);
+                }
+            }
+        }
+    }
+
+    unbindEvents() {
+        if (typeof this._eventBindings != 'undefined' && this._eventBindings != null && this._eventBindings.length > 0) {
+            let eventBinding
+            for (let i = 0; i < this._eventBindings.length; i++) {
+                eventBinding = this._eventBindings[i];
+                eventBinding.Dispacter.off(eventBinding.Type, eventBinding.Handler);
+                eventBinding.Bound = false;
+
+                dungeon_crawler.core.outputToConsole(`Event unbound: "${eventBinding.Name}"`);
+            }
+        }
+    }
+
+    clearBoundEvents() {
+        this._eventBindings = [];
+        this._eventBindingNames = [];
+    }
+}
