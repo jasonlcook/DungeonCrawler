23138d8735d92469f6311358dff4c809a332da6a
jasonlcook <tarquinnlcook@gmail.com>
2023-09-07 12:34:57 +0100

    Waterdeep monster list

---
+++ BlazorDungeonCrawler/UncompiledScripts/dungeon_crawler/functions/dungeon_crawler.enemies.js
@@ -0,0 +1,101 @@
+﻿dungeon_crawler.core.enemies = {
+    //https://stanislavs.tripod.com/games/eob1mons.htm
+    list: [
+        {
+            type: 'Kobold',
+            levelStart: 1, levelEnd: 2,
+            health: 10, strength: 7, armor: 12,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Kobold'
+        }, {
+            type: 'Giant Leech',
+            levelStart: 1, levelEnd: 1,
+            health: 27, strength: 15, armor: 13,
+            link: 'https://www.dandwiki.com/wiki/Giant_Leech_(5e_Creature)'
+        }, {
+            type: 'Skeleton',
+            levelStart: 2, levelEnd: 6,
+            health: 13, strength: 10, armor: 13,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Skeleton'
+        }, {
+            type: 'Zombie',
+            levelStart: 2, levelEnd: 6,
+            health: 22, strength: 13, armor: 8,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Zombie'
+        }, {
+            type: 'Kuo-Toa',
+            levelStart: 3, levelEnd: 3,
+            health: 18, strength: 13, armor: 13,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Kuo-Toa'
+        }, {
+            type: 'Flind',
+            levelStart: 3, levelEnd: 6,
+            health: 127, strength: 20, armor: 16,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Flind'
+        }, {
+            type: 'Giant Spider',
+            levelStart: 4, levelEnd: 5,
+            health: 26, strength: 14, armor: 14,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Giant-Spider'
+        }, {
+            type: 'Kenku',
+            levelStart: 6, levelEnd: 6,
+            health: 60, strength: 14, armor: 15,
+            link: 'https://www.dandwiki.com/wiki/Kenku_Blightwing_(5e_Creature)'
+        }, {
+            type: 'Drow Elf',
+            levelStart: 7, levelEnd: 9,
+            health: 71, strength: 13, armor: 18,
+            link: 'https://www.dandwiki.com/wiki/Drow_Elf,_Variant_(3.5e_Race)'
+        }, {
+            type: 'Skeletal Lord',
+            levelStart: 7, levelEnd: 9,
+            health: 142, strength: 16, armor: 13,
+            link: 'https://www.gmbinder.com/share/-LoH9Hgmov-rrNvVIKhh'
+        }, {
+            type: 'Drider',
+            levelStart: 8, levelEnd: 9,
+            health: 123, strength: 16, armor: 13,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Drider'
+        }, {
+            type: 'Hell Hound',
+            levelStart: 8, levelEnd: 9,
+            health: 45, strength: 17, armor: 15,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Hell-Hound'
+        }, {
+            type: 'Displacer Beast',
+            levelStart: 9, levelEnd: 9,
+            health: 85, strength: 18, armor: 13,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Displacer-Beast'
+        }, {
+            type: 'Rust Monster',
+            levelStart: 9, levelEnd: 9,
+            health: 27, strength: 13, armor: 14,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Rust-Monster'
+        }, {
+            type: 'Mantis Warrior',
+            levelStart: 10, levelEnd: 11,
+            health: 22, strength: 11, armor: 13,
+            link: 'https://www.dandwiki.com/wiki/Mantis_Warrior_(5e_Creature)'
+        }, {
+            type: 'Mind Flayer',
+            levelStart: 11, levelEnd: 11,
+            health: 71, strength: 11, armor: 15,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=mind-flayer'
+        }, {
+            type: 'Xorn',
+            levelStart: 11, levelEnd: 11,
+            health: 73, strength: 17, armor: 19,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Xorn'
+        }, {
+            type: 'Stone Golem',
+            levelStart: 12, levelEnd: 12,
+            health: 178, strength: 22, armor: 17,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=stone-golem'
+        }, {
+            type: 'Beholder',
+            levelStart: 12, levelEnd: 12,
+            health: 180, strength: 10, armor: 18,
+            link: 'https://www.aidedd.org/dnd/monstres.php?vo=Beholder'
+        }
+    ]
+}; 
