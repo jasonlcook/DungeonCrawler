fa4630b864d4a34df572365c83602c6122664fdc
jasonlcook <tarquinnlcook@gmail.com>
2023-09-14 10:44:50 +0100

    Add scale to enemy difficulty

--- BlazorDungeonCrawler/UncompiledScripts/dungeon_crawler/functions/dungeon_crawler.enemies.js
+++ BlazorDungeonCrawler/UncompiledScripts/dungeon_crawler/functions/dungeon_crawler.enemies.js
@@ -15,87 +15,87 @@ dungeon_crawler.core.enemies = {
         }, {
             type: 'Skeleton',
             levelStart: 2, levelEnd: 6,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 2, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Skeleton'
         }, {
             type: 'Zombie',
             levelStart: 2, levelEnd: 6,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 2, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Zombie'
         }, {
             type: 'Kuo-Toa',
             levelStart: 3, levelEnd: 3,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Kuo-Toa'
         }, {
             type: 'Flind',
             levelStart: 3, levelEnd: 6,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Flind'
         }, {
             type: 'Giant Spider',
             levelStart: 4, levelEnd: 5,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Giant-Spider'
         }, {
             type: 'Kenku',
             levelStart: 6, levelEnd: 6,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.dandwiki.com/wiki/Kenku_Blightwing_(5e_Creature)'
         }, {
             type: 'Drow Elf',
             levelStart: 7, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.dandwiki.com/wiki/Drow_Elf,_Variant_(3.5e_Race)'
         }, {
             type: 'Skeletal Lord',
             levelStart: 7, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.gmbinder.com/share/-LoH9Hgmov-rrNvVIKhh'
         }, {
             type: 'Drider',
             levelStart: 8, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Drider'
         }, {
             type: 'Hell Hound',
             levelStart: 8, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 3, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Hell-Hound'
         }, {
             type: 'Displacer Beast',
             levelStart: 9, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 4, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Displacer-Beast'
         }, {
             type: 'Rust Monster',
             levelStart: 9, levelEnd: 9,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 4, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Rust-Monster'
         }, {
             type: 'Mantis Warrior',
             levelStart: 10, levelEnd: 11,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 4, damageDice: 1, protectionDice: 1,
             link: 'https://www.dandwiki.com/wiki/Mantis_Warrior_(5e_Creature)'
         }, {
             type: 'Mind Flayer',
             levelStart: 11, levelEnd: 11,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 5, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=mind-flayer'
         }, {
             type: 'Xorn',
             levelStart: 11, levelEnd: 11,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 5, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Xorn'
         }, {
             type: 'Stone Golem',
             levelStart: 12, levelEnd: 12,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 6, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=stone-golem'
         }, {
             type: 'Beholder',
             levelStart: 12, levelEnd: 12,
-            healthDice: 1, damageDice: 1, protectionDice: 1,
+            healthDice: 7, damageDice: 1, protectionDice: 1,
             link: 'https://www.aidedd.org/dnd/monstres.php?vo=Beholder'
         }
     ],
