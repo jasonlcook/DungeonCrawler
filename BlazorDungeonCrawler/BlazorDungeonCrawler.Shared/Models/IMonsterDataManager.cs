8ba138c4ceefc2ec8c117747f35f2eaa82361aa6
jasonlcook <tarquinnlcook@gmail.com>
2023-09-24 22:25:05 +0100

    Remove Monster Controller and related items

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/IMonsterDataManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorDungeonCrawler.Shared.Models {
-    public interface IMonsterDataManager {
-        Task<List<Monster>> GetAllMonsters();
-        Task<Monster> GetMonster(int Id);
-        Task<List<Monster>> SearchMonsters(string Name);
-    }
-}
