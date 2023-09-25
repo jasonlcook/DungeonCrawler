e631c7d02f12f4814979c93653d913f0424ef523
jasonlcook <tarquinnlcook@gmail.com>
2023-09-25 11:14:38 +0100

    remove unused generic response type

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/ResponseEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorDungeonCrawler.Shared.Models {
-    public class APIEntityResponse<TEntity> where TEntity : class {
-        public bool Success { get; set; }
-        public List<string> ErrorMessages { get; set; } = new List<string>();
-        public TEntity? Data { get; set; }
-    }
-}
