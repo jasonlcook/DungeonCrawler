5656a836dc2c3e9cf0be3747a1b7fcbdc0adc76a
jasonlcook <tarquinnlcook@gmail.com>
2023-09-25 15:54:40 +0100

    Add dice rolls to message class

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Message.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Message.cs
@@ -1,7 +1,9 @@
 ﻿//Adventure message log 
 namespace BlazorDungeonCrawler.Shared.Models {
     public class Message {
+        public Guid Id { get; set; } = new Guid();
         public int Index { get; set; } = 0;
         public string Text { get; set; } = "";
+        public List<int> Dice { get; set; } = new List<int>();
     }
 }
