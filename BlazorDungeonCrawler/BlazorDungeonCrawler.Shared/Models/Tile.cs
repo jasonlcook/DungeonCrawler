c9f6986a9c8ccae7886795639e76803c13308f0b
jasonlcook <tarquinnlcook@gmail.com>
2023-09-26 18:56:07 +0100

    Add required properties to Tile class

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Tile.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Tile.cs
@@ -6,10 +6,18 @@ namespace BlazorDungeonCrawler.Shared.Models {
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
 
+
         public int Row { get; set; } = 0;
         public int Column { get; set; } = 0;
 
+        
         public DungeonEvemts Type { get; set; } = DungeonEvemts.Unknown;
+
+        
+        public bool Current { get; set; } = true;
+        public bool Hidden { get; set; } = true;
+        public bool Selectable { get; set; } = false;
+
 
         public List<Monster>? Monsters { get; set; } = null;
     }
