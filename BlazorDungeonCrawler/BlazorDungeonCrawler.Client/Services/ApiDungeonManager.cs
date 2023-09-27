ade00ed265ca840ebd92675a764dd40a1a1b42cd
jasonlcook <tarquinnlcook@gmail.com>
2023-09-27 13:50:10 +0100

    Set tile Html inline and add SelectTile function

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Client/Services/ApiDungeonManager.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Client/Services/ApiDungeonManager.cs
@@ -23,5 +23,18 @@ namespace BlazorDungeonCrawler.Client.Services {
 
             return new Dungeon();
         }
+
+        public async Task<Dungeon> SelectDungeonTile(Guid dungeonId, Guid tileId) {
+            var result = await httpClient.GetAsync($"https://localhost:7224/api/dungeon/{dungeonId}/tile/{tileId}");
+            result.EnsureSuccessStatusCode();
+
+            var response = await result.Content.ReadAsStringAsync();
+            var dungeonResponse = JsonConvert.DeserializeObject<DungeonResponse>(response);
+            if (dungeonResponse != null && dungeonResponse.Success) {
+                return dungeonResponse.Dungeon;
+            }
+
+            return new Dungeon();
+        }
     }
 }
