2f4edd0ff1f4802ecfb3b42a6795674eb89e1b4b
jasonlcook <tarquinnlcook@gmail.com>
2023-09-27 13:50:37 +0100

    remove explicit table link

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Database/DungeonCrawlerContext.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Database/DungeonCrawlerContext.cs
@@ -4,14 +4,13 @@ using BlazorDungeonCrawler.Shared.Models;
 
 namespace BlazorDungeonCrawler.Database {
     public class DungeonCrawlerContext : DbContext {
+        public DungeonCrawlerContext() {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
         public DbSet<Dungeon> Dungeons { get; set; }
         public DbSet<Adventurer> Adventurers { get; set; }
         public DbSet<Level> Levels { get; set; }
         public DbSet<Tile> Tiles { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
-            modelBuilder.Entity<Level>()
-                .HasMany<Tile>(l => l.Tiles);
-        }
     }
 }
