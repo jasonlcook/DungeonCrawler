8e4d138d34b44b2fb05fc479700b9d2c2da95359
jasonlcook <tarquinnlcook@gmail.com>
2023-09-28 19:51:57 +0100

    add MonsterType to database and remove monster manager

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Database/DungeonCrawlerContext.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Database/DungeonCrawlerContext.cs
@@ -4,13 +4,15 @@ using BlazorDungeonCrawler.Shared.Models;
 
 namespace BlazorDungeonCrawler.Database {
     public class DungeonCrawlerContext : DbContext {
-        public DungeonCrawlerContext() {
-            Configuration.LazyLoadingEnabled = false;
+        public DungeonCrawlerContext() : base(){
+            this.Configuration.LazyLoadingEnabled = false;
         }
 
         public DbSet<Dungeon> Dungeons { get; set; }
         public DbSet<Adventurer> Adventurers { get; set; }
         public DbSet<Level> Levels { get; set; }
         public DbSet<Tile> Tiles { get; set; }
+
+        public DbSet<MonsterType> MonsterType { get; set; }
     }
 }
