1ae94ae4a9b9dde9abef4d3995cfa4848049339b
jasonlcook <tarquinnlcook@gmail.com>
2023-10-04 20:29:45 +0100

    Add monster types as local classes

---
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Server/Data/MonsterType.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDungeonCrawler.Shared.Models {
+
+    //todo: add how many monsters in a group
+    public class MonsterType {
+        [Key]
+        public Guid Id { get; set; }
+
+
+        public string Name { get; set; } 
+
+
+        public int LevelStart { get; set; }
+        public int LevelEnd { get; set; }
+
+
+        public int HealthDiceCount { get; set; }
+        public int DamageDiceCount { get; set; }
+        public int ProtectionDiceCount { get; set; } 
+
+
+        public string Documentation { get; set; }
+
+
+        public MonsterType() {
+            Id = Guid.Empty;
+            Name = string.Empty;
+            Documentation = string.Empty;
+
+        }
+    }
+}
