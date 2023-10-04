40c6b1916bcb633201df22c082a1396a988371b1
jasonlcook <tarquinnlcook@gmail.com>
2023-10-04 13:19:16 +0100

    expilcity set constructor

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Responses/DungeonResponse.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Responses/DungeonResponse.cs
@@ -4,9 +4,14 @@ namespace BlazorDungeonCrawler.Shared.Responses
 {
     public class DungeonResponse
     {
-        public bool Success { get; set; } = true;
-        public List<string> ErrorMessages { get; set; } = new List<string>();
-        public Dungeon Dungeon { get; set; } = new Dungeon();
-    }
+        public bool Success { get; set; } 
+        public List<string> ErrorMessages { get; set; } 
+        public Dungeon Dungeon { get; set; } 
 
+        public DungeonResponse()
+        {
+            ErrorMessages = new();
+            Dungeon = new();
+        }
+    }
 }
