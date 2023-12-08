d435995e23512dd491d3821f016746de2388299a
jasonlcook <tarquinnlcook@gmail.com>
2023-12-08 07:44:18 +0000

    Fix tile header

--- BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Tile.cs
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/Tile.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 using BlazorDungeonCrawler.Shared.Enumerators;
 
 namespace BlazorDungeonCrawler.Shared.Models {
