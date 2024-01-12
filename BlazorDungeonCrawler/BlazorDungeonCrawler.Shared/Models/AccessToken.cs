a1574196b56b06732c1cffb224e51322cb38904e
jasonlcook <tarquinnlcook@gmail.com>
2024-01-12 07:31:48 +0000

    Add shared data models for Access Token

---
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/AccessToken.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDungeonCrawler.Shared.Models {
+    public class AccessToken {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Token { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public AccessToken() {
+            Id = Guid.Empty;
+
+            Token = string.Empty;
+            Name = string.Empty;
+
+            CreateDate = DateTime.MinValue;
+        }
+
+        public AccessToken(string accessToken, string name) {
+            Id = Guid.NewGuid();
+
+            Token = accessToken;
+            Name = name;
+
+            CreateDate = DateTime.Now;
+        }
+    }
+}
---
+++ BlazorDungeonCrawler/BlazorDungeonCrawler.Shared/Models/AccessTokenLog.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorDungeonCrawler.Shared.Models {
+    public class AccessTokenLog {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime DateAccessed { get; set; }
+
+        [ForeignKey("AccessToken")]
+        public Guid AccessTokenId { get; set; }
+
+        public AccessTokenLog() {
+            Id = Guid.NewGuid();
+            DateAccessed = DateTime.Now;
+        }
+    }
+}
